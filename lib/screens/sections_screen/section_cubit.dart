0d22ecb004c0aa3d926d0734982d9facdc9fc9c2
Shaban <sh3ban.kamel@gmail.com>
2023-03-15 23:43:19 +0200

    Improve code

--- lib/screens/sections_screen/section_cubit.dart
+++ lib/screens/sections_screen/section_cubit.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../zad_states.dart';
@@ -7,10 +6,4 @@ class ZadCubit extends Cubit<ZadStates> {
   ZadCubit() : super(ZadInitialState());
 
   static ZadCubit get(context) => BlocProvider.of(context);
-
-  int currentIndex = 0;
-
-  Future<String> loadAsset() async {
-    return await rootBundle.loadString('assets/file.txt');
-  }
 }
