73781a091df695d943905ac9b8c7d468c16204a6
NagyElseady <nagyelsead@gmail.com>
2023-03-26 16:59:12 +0200

    do URL launch

--- lib/sidemenu_option/references_screen.dart
+++ lib/sidemenu_option/references_screen.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:zad/shared/localization/localizations.dart';
 
 class ReferencesScreen extends StatelessWidget {
   const ReferencesScreen({Key? key}) : super(key: key);
+
+  launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +38,15 @@ class ReferencesScreen extends StatelessWidget {
           ),
           Text(localizations.source_syntax1),
           Text(localizations.source_syntax2),
+          ElevatedButton(
+              onPressed: () {
+                const url = 'http://www.islamsbudskap.com/ig/khutba/k/';
+                launchURL(url);
+              },
+              child: Text(
+                localizations.url,
+                style: const TextStyle(color: Colors.blue),
+              ))
         ],
       ),
     );
